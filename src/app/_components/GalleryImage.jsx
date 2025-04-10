658e95a9808bae79d35168f75416f258057ac431
Brett Boettcher <hgdxg@BrettsRTX>
2025-04-10 00:39:30 -0600

    working on formatting main image view

--- src/app/_components/GalleryImage.jsx
+++ src/app/_components/GalleryImage.jsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function GalleryImage({ description, id, src }) {
 	return (
 		<Link href={`/categories/${id}`}>
-			<div className="flex flex-col gap-y-1 items-center justify-center w-fit h-fit">
-				<Image src={src} alt={description} width={5000} height={5000} className="w-full h-full rounded-sm object-contain" />
+			<div className="flex flex-col gap-y-1 items-center justify-center">
+				<Image src={src} alt={description} width={5000} height={5000} className="rounded-sm" />
 				<h5 className="text-md sm:text-lg leading-tight font-semibold text-gray-200 text-center w-9/10">{description}</h5>
 			</div>
 		</Link>
--- src/app/image/[id]/page.jsx
+++ src/app/image/[id]/page.jsx
@@ -3,7 +3,7 @@ import GalleryImage from "@/app/_components/GalleryImage";
 export default function ViewImage() {
 	return (
 		<div className="flex flex-col gap-3 p-3">
-			<GalleryImage description={"Animals In Nature"} src="/images/green-bird.jpg" id={"a"} />
+			<GalleryImage description={"Animals In Nature"} src="/images/bird.webp" id={"a"} />
 		</div>
 	);
 }
